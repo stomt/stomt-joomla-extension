63f582b1c94c250899bb3779acafaf2412d89c40
bader mariem <bader.mariem1@gmail.com>
2018-02-14 21:24:49 +0100

    configuration

--- mod_STOMT/mod_STOMT.php
+++ mod_STOMT/mod_STOMT.php
@@ -1,31 +1,32 @@
 <?php
-/**
- * @package     Joomla.Site
- * @subpackage  mod_breadcrumbs
- *
- * @copyright   Copyright (C) 2005 - 2018 Open Source Matters, Inc. All rights reserved.
- * @license     GNU General Public License version 2 or later; see LICENSE.txt
- */
+/*------------------------------------------------------------------------
+# mod_sp_simple_youtube - Youtube Module by JoomShaper.com
+# ------------------------------------------------------------------------
+# author    JoomShaper http://www.joomshaper.com
+# Copyright (C) 2010 - 2014 JoomShaper.com. All Rights Reserved.
+# @license - http://www.gnu.org/licenses/gpl-2.0.html GNU/GPL
+# Websites: http://www.joomshaper.com
+-------------------------------------------------------------------------*/
 
-defined('_JEXEC') or die;
+// no direct access
+defined('_JEXEC') or die('Restricted access');
+//Parameters
+$doc 					= JFactory::getDocument();
+$uniqid 				= $module->id;
+$stomt_id				= $params->get ('stomt_id');
+$stomt_label			= $params->get ('stomt_label');
+$stomt_color_text		= $params->get ('color_text');
+$responsive				= $params->get('responsive', 1);
+$showclose				= $params->get('showclose', 1);
 
 
-?>
+// Load the method jquery script.
+if( $responsive )
+{
+	JHtml::_('jquery.framework');
+	?>
+	
+	<?php
+}
 
-<script>
-  // Include the STOMT JavaScript SDK
-  (function(w, d, n, r, t, s){
-    w.Stomt = w.Stomt||[];
-    t = d.createElement(n);
-    s = d.getElementsByTagName(n)[0];
-    t.async=1;
-    t.src=r;
-    s.parentNode.insertBefore(t,s);
-  })(window, document, 'script', 'https://www.stomt.com/widget.js');
-  
-  // Adjust the 'APP_ID' to your application id 
-  // you can find it here: https://www.stomt.com/YOUR_PAGE/apps
-  Stomt.push(['addTab', {appId: 'APP_ID'}]);
-  Stomt.push(['addFeed', {appId: 'APP_ID'}]);
-  Stomt.push(['addCreate', {appId: 'APP_ID'}]);
-</script>
+require(JModuleHelper::getLayoutPath('mod_stomt'));
